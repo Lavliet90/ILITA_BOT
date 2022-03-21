3b79edea55d1cef11d8856817472b079f572d846
Lavliet90 <60854775+Lavliet90@users.noreply.github.com>
2022-03-21 16:05:37 +0100

    Update main.py

--- main.py
+++ main.py
@@ -45,36 +45,3 @@ def webhook():
 
 if __name__ == '__main__':
     server.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
-# import os
-# import telebot
-# import logging
-# from ilitaconfig import token_telegram, app_url
-# from flask import Flask, request
-#
-# bot = telebot.TeleBot(token_telegram)
-# server = Flask(__name__)
-# logger = telebot.logger
-# logger.setLevel(logging.DEBUG)
-#
-#
-# @bot.message_handler(commands=['start'])
-# def start(message):
-#     username = message.from_user.first_name
-#     bot.reply_to(message, f"Hello, {username}!")
-#
-#
-# @server.route(f'/{token_telegram}', methods=['POST'])  # перенаправление сообщений сервера фласк к телеграм боту
-# def redirect_message():
-#     json_string = request.get_data().decode('utg-8')  # получаем данные от сервера в json
-#     update = telebot.types.Update.de_json(json_string)
-#     bot.process_new_updates([update])  # перенаправляем к боту
-#     return '!', 200
-#
-
-
-
-# bot.infinity_polling()
-# if __name__ == "__main__":
-#     bot.remove_webhook()
-#     bot.set_webhook(url=app_url)
-#     server.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
