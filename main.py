f6fe80f312e0e4f59ccae5f29718508d43289742
lavliet90 <lavliet90@mail.ru>
2022-03-21 15:44:07 +0100

    fix bag host3

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ import logging
 from ilitaconfig import token_telegram, app_url
 from flask import Flask, request
 
-
 bot = telebot.TeleBot(token_telegram)
 server = Flask(__name__)
 logger = telebot.logger
@@ -16,12 +15,15 @@ def start(message):
     username = message.from_user.first_name
     bot.reply_to(message, f"Hello, {username}!")
 
-@server.route(f'/{token_telegram}', methods=['POST']) #перенаправление сообщений сервера фласк к телеграм боту
+
+@server.route(f'/{token_telegram}', methods=['POST'])  # перенаправление сообщений сервера фласк к телеграм боту
 def redirect_message():
-    json_string = request.get_data().decode('utg-8') #получаем данные от сервера в json
+    json_string = request.get_data().decode('utg-8')  # получаем данные от сервера в json
     update = telebot.types.Update.de_json(json_string)
-    bot.process_new_updates([update])   #перенаправляем к боту
+    bot.process_new_updates([update])  # перенаправляем к боту
     return '!', 200
+
+
 #
 # @bot.message_handler(func=lambda m: True)
 # def test_pinging(message):
