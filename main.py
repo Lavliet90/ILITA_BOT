4ab2e2780bb16b1afac02cbc0706f1ec6f59fe55
lavliet90 <lavliet90@mail.ru>
2022-03-21 16:47:54 +0100

    Sorry for what?

--- main.py
+++ main.py
@@ -21,9 +21,10 @@ def test_pinging(message):
         bot.send_message(message.chat.id, "буп")
     elif 'соси' in message.text.lower() or 'sosi' in message.text.lower() or \
             'саси' in message.text.lower() or 'sasi' in message.text.lower():
-        print(message.text.lower())
-        bot.reply_to(message,
-                     f'Сам соси, {message.from_user.first_name}')  # Только для беседы, в личке не from_user, a chat
+        bot.reply_to(message, f'Сам соси, {message.from_user.first_name}')
+        # Только для беседы, в личке не from_user, a chat
+    elif 'извини' in message.text.lower() or 'sorry' in  message.text.lower():
+        bot.reply_to(message, f'Sorry for what {message.from_user.first_name}?')
     else:
         pass
 
@@ -45,36 +46,3 @@ def webhook():
 
 if __name__ == '__main__':
     server.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
-# import os
-# import telebot
-# import logging
-# from ilitaconfig import token_telegram, app_url
-# from flask import Flask, request
-#
-# bot = telebot.TeleBot(token_telegram)
-# server = Flask(__name__)
-# logger = telebot.logger
-# logger.setLevel(logging.DEBUG)
-#
-#
-# @bot.message_handler(commands=['start'])
-# def start(message):
-#     username = message.from_user.first_name
-#     bot.reply_to(message, f"Hello, {username}!")
-#
-#
-# @server.route(f'/{token_telegram}', methods=['POST'])  # перенаправление сообщений сервера фласк к телеграм боту
-# def redirect_message():
-#     json_string = request.get_data().decode('utg-8')  # получаем данные от сервера в json
-#     update = telebot.types.Update.de_json(json_string)
-#     bot.process_new_updates([update])  # перенаправляем к боту
-#     return '!', 200
-#
-
-
-
-# bot.infinity_polling()
-# if __name__ == "__main__":
-#     bot.remove_webhook()
-#     bot.set_webhook(url=app_url)
-#     server.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
