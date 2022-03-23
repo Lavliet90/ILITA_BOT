f356e0e6167cf06d815c8340f00c871dec674c0a
lavliet90 <lavliet90@mail.ru>
2022-03-23 16:05:43 +0100

    gitignotr test2

--- ilitaconfig.py
+++ ilitaconfig.py
@@ -1,3 +1,4 @@
 token_telegram = '5184779214:AAGWSKy7Nl8xtZ2V8DGiEwyADhJklELrXnY'
 app_url = f'https://ilitabot.herokuapp.com//{token_telegram}'
 db_uri = 'postgres://btelsnuqevdwua:c2c3beb1821bc6a8b019d1e29cd9b0440189520cb3aacdd98096fb33182a5e58@ec2-34-242-89-204.eu-west-1.compute.amazonaws.com:5432/d6pb467f4tpkp3'
+print('dfwfe')
