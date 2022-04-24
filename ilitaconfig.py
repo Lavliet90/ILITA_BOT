19104c4b9d13e254178a696ff185c41c56369694
Yan <bbb.zzzz.bbb@mail.ru>
2022-04-24 11:09:07 +0200

    cdsfw

--- ilitaconfig.py
+++ ilitaconfig.py
@@ -1,3 +1,3 @@
 token_telegram='5184779214:AAGWSKy7Nl8xtZ2V8DGiEwyADhJklELrXnY'
 app_url = 'https://ilitabot.herokuapp.com/' + token_telegram
-db_uri = 'postgres://btelsnuqevdwua:c2c3beb1821bc6a8b019d1e29cd9b0440189520cb3aacdd98096fb33182a5e58@ec2-34-242-89-204.eu-west-1.compute.amazonaws.com:5432/d6pb467f4tpkp3'
+db_uri = 'postgres://lcurjymkznmwcp:55bfdefc82d47ec13fbd872534f0f182a6003ed8f32c80469c5c6430b0fe1aff@ec2-63-32-248-14.eu-west-1.compute.amazonaws.com:5432/d6nmgh4apolhlh'
