74eb50f5e4b9a1609b95b652fdab3bd296645d09
Yan <bbb.zzzz.bbb@mail.ru>
2022-05-11 19:37:44 +0200

    refactor db

--- ilitaconfig.py
+++ ilitaconfig.py
@@ -1,3 +1,3 @@
 token_telegram='5184779214:AAGWSKy7Nl8xtZ2V8DGiEwyADhJklELrXnY'
 app_url = 'https://ilitabot.herokuapp.com/' + token_telegram
-db_uri = 'postgres://lcurjymkznmwcp:55bfdefc82d47ec13fbd872534f0f182a6003ed8f32c80469c5c6430b0fe1aff@ec2-63-32-248-14.eu-west-1.compute.amazonaws.com:5432/d6nmgh4apolhlh'
+db_uri = 'postgres://xhnxmqooheszoc:09d855a2f2dd2166fbf24ebd165d7dfadb26411b4a8db81bb78ab96ea800ebc9@ec2-52-18-116-67.eu-west-1.compute.amazonaws.com:5432/d9pfn3sjdfv9hf'
