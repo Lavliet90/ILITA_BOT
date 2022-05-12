b6786bba43bd82febf094eaf71caab730e939bce
Yan Alsheuski <60854775+Lavliet90@users.noreply.github.com>
2022-05-12 14:41:35 +0200

    config

--- ilitaconfig.py
+++ ilitaconfig.py
@@ -1,3 +1,3 @@
-token_telegram='5184779214:AAGWSKy7Nl8xtZ2V8DGiEwyADhJklELrXnY'
-app_url = 'https://ilitabot.herokuapp.com/' + token_telegram
-db_uri = 'postgres://xhnxmqooheszoc:09d855a2f2dd2166fbf24ebd165d7dfadb26411b4a8db81bb78ab96ea800ebc9@ec2-52-18-116-67.eu-west-1.compute.amazonaws.com:5432/d9pfn3sjdfv9hf'
+token_telegram = 'TOKEN' #take it from the BotFather
+app_url = 'LINK' + token_telegram  #get link  Settings/Domnains
+db_uri = 'db uri' #after create db in heroku, go into it/Settings/Database Crentials
